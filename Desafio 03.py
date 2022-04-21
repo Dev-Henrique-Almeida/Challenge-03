a58ca57e485d0393666b6b36a58508e31fea54fe
Henrique Almeida <101284170+Dev-Henrique-Almeida@users.noreply.github.com>
2022-04-21 14:03:16 -0300

    Challenge #03
    Desafio 03, curso de Python

---
+++ Desafio 03.py
@@ -0,0 +1,9 @@
+print('===== DESAFIO  03 ======')
+
+num1 = int(input('Digite um numero:'))
+num2 = int(input('Digite um numero:'))
+
+print('Primeiro número', num1)
+print('Segundo número', num2)
+
+print('A soma é ', num1 + num2)
